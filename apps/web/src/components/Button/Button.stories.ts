aa0820e67bd3132ccc5dc390b2d5bf3acab37b03
Mehrad Rousta <mehrad@protonmail.com>
2024-11-09 11:27:14 +0330

    docs(Button): update button story

--- apps/web/src/components/Button/Button.stories.ts
+++ apps/web/src/components/Button/Button.stories.ts
@@ -38,195 +38,62 @@ const meta: Meta<typeof Button> = {
 
 export default meta;
 type Story = StoryObj<typeof meta>;
-
-/**
- * Primary Buttons
- */
-export const PrimarySolid: Story = {
+export const Primary: Story = {
   args: {
     color: "primary",
-    variant: "solid",
+  },
+  argTypes: {
+    variant: {
+      control: {
+        type: "select",
+        options: ["solid", "outline", "text"],
+      },
+    },
+    disabled: { control: "boolean" },
   },
 };
 
-export const PrimaryOutline: Story = {
-  args: {
-    color: "primary",
-    variant: "outline",
-  },
-};
-
-export const PrimaryText: Story = {
-  args: {
-    color: "primary",
-    variant: "text",
-  },
-};
-
-export const PrimarySolidDisabled: Story = {
-  args: {
-    color: "primary",
-    variant: "solid",
-    disabled: true,
-  },
-};
-
-export const PrimaryOutlineDisabled: Story = {
-  args: {
-    color: "primary",
-    variant: "outline",
-    disabled: true,
-  },
-};
-
-export const PrimaryTextDisabled: Story = {
-  args: {
-    color: "primary",
-    variant: "text",
-    disabled: true,
-  },
-};
-
-/**
- * Secondary Buttons
- */
-export const SecondarySolid: Story = {
+export const Secondary: Story = {
   args: {
     color: "secondary",
-    variant: "solid",
+  },
+  argTypes: {
+    variant: {
+      control: {
+        type: "select",
+        options: ["solid", "outline", "text"],
+      },
+    },
+    disabled: { control: "boolean" },
   },
 };
 
-export const SecondaryOutline: Story = {
-  args: {
-    color: "secondary",
-    variant: "outline",
-  },
-};
-
-export const SecondaryText: Story = {
-  args: {
-    color: "secondary",
-    variant: "text",
-  },
-};
-
-export const SecondarySolidDisabled: Story = {
-  args: {
-    color: "secondary",
-    variant: "solid",
-    disabled: true,
-  },
-};
-
-export const SecondaryOutlineDisabled: Story = {
-  args: {
-    color: "secondary",
-    variant: "outline",
-    disabled: true,
-  },
-};
-
-export const SecondaryTextDisabled: Story = {
-  args: {
-    color: "secondary",
-    variant: "text",
-    disabled: true,
-  },
-};
-
-/**
- * Black Buttons
- */
-export const BlackSolid: Story = {
+export const Black: Story = {
   args: {
     color: "black",
-    variant: "solid",
+  },
+  argTypes: {
+    variant: {
+      control: {
+        type: "select",
+        options: ["solid", "outline", "text"],
+      },
+    },
+    disabled: { control: "boolean" },
   },
 };
 
-export const BlackOutline: Story = {
-  args: {
-    color: "black",
-    variant: "outline",
-  },
-};
-
-export const BlackText: Story = {
-  args: {
-    color: "black",
-    variant: "text",
-  },
-};
-
-export const BlackSolidDisabled: Story = {
-  args: {
-    color: "black",
-    variant: "solid",
-    disabled: true,
-  },
-};
-
-export const BlackOutlineDisabled: Story = {
-  args: {
-    color: "black",
-    variant: "outline",
-    disabled: true,
-  },
-};
-
-export const BlackTextDisabled: Story = {
-  args: {
-    color: "black",
-    variant: "text",
-    disabled: true,
-  },
-};
-
-/**
- * White Buttons
- */
-export const WhiteSolid: Story = {
+export const White: Story = {
   args: {
     color: "white",
-    variant: "solid",
   },
-};
-
-export const WhiteOutline: Story = {
-  args: {
-    color: "white",
-    variant: "outline",
-  },
-};
-
-export const WhiteText: Story = {
-  args: {
-    color: "white",
-    variant: "text",
-  },
-};
-
-export const WhiteSolidDisabled: Story = {
-  args: {
-    color: "white",
-    variant: "solid",
-    disabled: true,
-  },
-};
-
-export const WhiteOutlineDisabled: Story = {
-  args: {
-    color: "white",
-    variant: "outline",
-    disabled: true,
-  },
-};
-
-export const WhiteTextDisabled: Story = {
-  args: {
-    color: "white",
-    variant: "text",
-    disabled: true,
+  argTypes: {
+    variant: {
+      control: {
+        type: "select",
+        options: ["solid", "outline", "text"],
+      },
+    },
+    disabled: { control: "boolean" },
   },
 };
